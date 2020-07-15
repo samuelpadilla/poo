8015e3cd04000924388ac068a4154324f22f7aa7
Samuel Padilla Latance <samuel.padilla@congregacao.org.br>
2020-07-15 20:04:33 -0300

    Segundo commit

---
+++ Pessoa.cs
@@ -0,0 +1,20 @@
+
+namespace poo
+{
+    class Pessoa 
+    {
+        public string nome;
+        int idade;
+        private float peso;
+        bool sexo;
+        string cor_olhos;
+
+        void andar(int velocidade){
+
+        }
+
+        float retornaPeso() {
+            return 1.0f;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            Pessoa objetoPessoa = new Pessoa();
+            objetoPessoa.nome = "Samuel";
+
+            Console.WriteLine(objetoPessoa.nome);
+            Console.ReadKey();
+        }
+    }
+}
